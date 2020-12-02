25ba6a9908512d3c52d1ca350d015a5adaf73d0a
Rese574 <therese.bolabola@cit.edu>
2020-12-02 10:10:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/finalb_rushadmin/AddDriver.java
@@ -0,0 +1,14 @@
+package com.example.finalb_rushadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddDriver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_driver);
+    }
+}
