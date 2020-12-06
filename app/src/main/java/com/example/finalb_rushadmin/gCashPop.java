aef4cd833b662a7b86ad870927d110ed601f2e2c
paradoxials <70249884+paradoxials@users.noreply.github.com>
2020-12-06 16:15:04 +0800

    slight changes for Pao

--- app/src/main/java/com/example/finalb_rushadmin/gCashPop.java
+++ app/src/main/java/com/example/finalb_rushadmin/gCashPop.java
@@ -9,21 +9,11 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class gCashPop extends AppCompatActivity {
-    private Button showSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_g_cash_pop);
-        showSearch = (Button) findViewById(R.id.testSearch);
-
-        showSearch.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){ openBusSearch();}
-        });
     }
 
-    public void openBusSearch(){
-        Intent intent = new Intent(this, BusSearch.class);
-        startActivity(intent);
-    }
+
 }
